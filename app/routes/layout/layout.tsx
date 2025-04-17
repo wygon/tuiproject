f23e38218c6b77a088e235b2c8045ae877aeff55
wygon <szymon302.sws@gmail.com>
2025-04-17 14:14:34 +0200

    1.0.2

--- app/routes/layout/layout.tsx
+++ app/routes/layout/layout.tsx
@@ -1,22 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "app/routes/airbnbcss/stylesheet.css";
 import { PiAppStoreLogoBold, PiArticleThin, PiGlobe, PiHeartStraightBold, PiPersonSimpleCircleDuotone } from "react-icons/pi";
 import { Outlet } from 'react-router';
 import CategoryNav from '~/components/CategoryNav';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+function getWindowSize() {
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function handleResize() { 
+            setWindowSize(window.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resiuze', handleResize);
+    }, []);
+    return windowSize;
+}
 
 export default function (){
     const [activeSearchHome, setActiveSearchHome] =  useState(true);
+    const [searchBarActive, setSearchBarActive] =  useState(false);
+    const currentWidth = getWindowSize();
     return (
         <div>
         <nav className="bg-light">
             <div className="row">
-                <div className="col-4">
-                    <button className="btn p-3"><PiAppStoreLogoBold /></button>
+                <div className="col-4 d-flex align-items-center">
+                    <span className="p-3 ps-5 logo d-flex align-items-center"><PiAppStoreLogoBold />{currentWidth > 1000 ? "Airbnb" : ""}</span>
                 </div>
                 <div className="col-4 text-center p-3">
-                    <button className="btn" onClick={() => setActiveSearchHome(true)}>Domy</button>
-                    <button className="btn" onClick={() => setActiveSearchHome(false)}>Atrakcje</button>
+                    <button className="btn" style={{fontWeight: activeSearchHome ? '600' : 'normal'}} onClick={() => setActiveSearchHome(true)}>Domy</button>
+                    <button className="btn" style={{fontWeight: !activeSearchHome ? '600' : 'normal'}}onClick={() => setActiveSearchHome(false)}>Atrakcje</button>
                 </div>
                 <div className="col-4 text-right p-2  d-flex align-items-center justify-content-end">
                     <button className="btn">Wynajmij swoj dom na Aribnb</button> 
@@ -27,45 +47,47 @@ export default function (){
                     </button>
                 </div>
                 <div className="d-flex justify-content-center align-items-center">
-                    <div className="search-bar col-sm-12 col-md-12 col-lg-12 col-xl-6 text-center bg-white rounded-pill p-2">
+                    <div className="search-bar bg-white rounded-pill" onFocus={() => setSearchBarActive(true)} onBlur={() => setSearchBarActive(false)}>
                         <form className="d-flex align-items-center w-100" >
-                            <span className="rounded-pill search-bar-item p-2 m-1 pe-5">
-                                <label htmlFor="kierunki" className="pe-5">Gdzie</label>
-                                <input id="kierunki" className="pe-5"  placeholder="Wyszukaj kierunki" />
-                            </span>
-
+                            <label htmlFor="kierunki" className="rounded-pill search-bar-item p-2 w33">
+                                <span className="">Gdzie</span>
+                                <input id="kierunki" className=""  placeholder="Wyszukaj kierunki" />
+                            </label>
                             <div className="divider"></div>
                             {activeSearchHome ? (
                             <>
-                                <span className="rounded-pill search-bar-item p-2 m-1">
-                                    <label htmlFor="przyjazd" >Przyjazd</label>
+                            <div className="w33 d-flex align-items-center">
+                                <label htmlFor="przyjazd" className="rounded-pill search-bar-item p-2 w-50">
+                                    <span>Przyjazd</span>
                                     <input id="przyjazd" className="" placeholder="Dodaj daty" />
-                                </span>
-
+                                </label>
                                 <div className="divider"></div>
-                                
-                                <span className="rounded-pill search-bar-item p-2 m-1">
-                                    <label htmlFor="wyjazd">Wyjazd</label>
+                                <label htmlFor="wyjazd" className="rounded-pill search-bar-item p-2 w-50">
+                                    <span >Wyjazd</span>
                                     <input id="wyjazd" className="" placeholder="Dodaj daty" />
-                                </span>
-                                <div className="divider" />
+                                </label>
+                            </div>
                             </>
                             ) : (
                             <>
-                                <span className="rounded-pill search-bar-item p-2 m-1 me-4">
-                                    <label htmlFor="przyjazd" >Data</label>
+                                <label htmlFor="przyjazd" className="rounded-pill search-bar-item p-2 w33">
+                                    <span >Data</span>
                                     <input id="przyjazd" className="" placeholder="Dodaj daty" />
-                                </span>
-                                <div className="divider" />
+                                </label>
                             </>
                             )}
-                            
-                            
-                            <span className="rounded-pill search-bar-item p-2 m-1">
-                                <label  htmlFor="kto" className="me-2">Kto</label>
-                                <input id="kto" className="me-2" placeholder="Dodaj gości" />
-                            </span>
-                            <button className="btn rounded-pill bg-danger p-3 ms-auto text-center"><PiHeartStraightBold/></button>
+                            <div className="divider" />
+                            <label htmlFor="kto" className="rounded-pill search-bar-item p-2 d-flex flex-row justify-content-between align-items-center w33">
+                                <div className="d-flex flex-column">
+                                    <span className="me-2">Kto</span>
+                                    <input id="kto" className="me-2" placeholder="Dodaj gości" />
+                                </div>
+                                {!searchBarActive ? 
+                                (<button className="btn rounded-pill bg-danger p-2 text-center h-100"><PiHeartStraightBold/></button>)
+                                : 
+                                (<button className="btn rounded-pill bg-danger p-2 text-center h-100 d-flex align-items-center"><PiHeartStraightBold/><span>Szukaj</span></button>)}
+                                {/* <button className="btn rounded-pill bg-danger p-2 text-center h-100"><PiHeartStraightBold/></button> */}
+                            </label>
                         </form>
                     </div>
                 </div>
@@ -78,7 +100,6 @@ export default function (){
         </nav>
         <Outlet></Outlet>
         <h1>STOPKA</h1>
-        <script src="bootstrap\dist\js\bootstrap.bundle.min.js" />
         </div>
     );
 }
